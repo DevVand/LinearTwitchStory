b55e521c31bd7c55129dea91a0acd3f5f6ce6990
LinearHaru <61861793+LinearHaru@users.noreply.github.com>
2022-03-22 11:25:48 -0300

    adjetivos

---
+++ adventure/Assets/assets/scripts/Adjetivo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adjetivo: MonoBehaviour
+{
+    public string nome = "ADJETIVO";
+    public int fixo = 0;
+}
